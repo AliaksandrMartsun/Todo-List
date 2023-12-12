9ce276a97e3d9d287d3734e5dc35ff25cf1f2715
AliaksandrMartsun <Aliaksandr.martsun@gmail.com>
2023-12-12 14:09:20 +0300

    new functions

--- todo/admin.py
+++ todo/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Todo, Category
 
+
 # Register your models here.
 
-
+@admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'description', 'date', 'done')
     list_display_links = ('id', 'title')
@@ -12,11 +13,8 @@ class TodoAdmin(admin.ModelAdmin):
     list_filter = ('done', 'category')
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('title',)
     search_fields = ('title',)
-
-
-admin.site.register(Todo, TodoAdmin)
-admin.site.register(Category, CategoryAdmin)
