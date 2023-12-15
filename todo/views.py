9ddbe8153fb0f43b12e3cf76e0c434e6e66f7ab4
AliaksandrMartsun <Aliaksandr.martsun@gmail.com>
2023-12-15 17:18:58 +0300

    add categories

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Todo, Category
+
+
+class TodoView(ListView):
+    model = Todo
+    queryset = Todo.objects.filter(draft=False)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["categories"] = Category.objects.all
+        return context
